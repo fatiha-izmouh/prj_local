83373f675ae868f3681d1ee4f48f6e71e8fafe1c
MohamedSouilhi <souilhimohamed02@gmail.com>
2024-10-07 09:38:34 +0200

    adding mapp

--- lib/pages/map.dart
+++ lib/pages/map.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
 
 class MapPage extends StatefulWidget {
   const MapPage({super.key});
@@ -9,13 +11,38 @@ class MapPage extends StatefulWidget {
 }
 
 class _MapPageState extends State<MapPage> {
+  static const String token = 'pk.eyJ1IjoibWVkc291aSIsImEiOiJjbTFwZ3UxZWowNGVmMmlzanlieWRzdDI5In0.new4dhJzrpAH5rv-7gM1Bg';
+  static const String urlTemp = 'https://api.mapbox.com/styles/v1/mapbox/streets-v12/tiles/256/{z}/{x}/{y}?access_token=$token';
+  final MapController _mapController = MapController();
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.grey[200]!,
+        debugShowCheckedModeBanner: false,
+      home:  Scaffold(
+        body:Stack(
+          children: [
+            FlutterMap(
+              mapController: _mapController,
+              options: MapOptions(
+                  initialCenter: LatLng(27.055366, -13.424025),
+                  initialZoom: 16.7,
+                  onTap: (tapPosition,LatLng){
 
-      ),
+                  }
+              ),
+              children: [
+                TileLayer(
+                  urlTemplate: urlTemp,
+                  fallbackUrl: urlTemp,
+                  //urlTemplate:"http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" ,
+                ),
+
+              ],
+            ),
+
+          ],
+        ) ,
+      )
     );
   }
 }
